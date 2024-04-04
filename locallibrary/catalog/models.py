37fcb0d8acd0464f0734fc49819c9a822ed37ada
Zane Jude <zjude04@gmail.com>
2024-04-03 19:21:11 -0500

    part 6, 7, and 8

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,6 +5,8 @@ from django.urls import reverse # Used in get_absolute_url() to get URL for spec
 
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+from django.conf import settings
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -77,6 +79,7 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     language = models.CharField(max_length=100, default='English')
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -99,6 +102,12 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+    #"""Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
 class Author(models.Model):
     """Model representing an author."""
@@ -117,6 +126,9 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+    
+    class Meta:
+        ordering = ['last_name']
 
 class Language(models.Model):
     """Model representing a language."""
